a18cddb668203e055c06ecf62036c2320171c01d
Aditya Dubey <aditya125.ad@gmail.com>
2020-11-07 15:43:40 -0500

    api stuff

--- apiEndpoint/server.js
+++ apiEndpoint/server.js
@@ -19,72 +19,99 @@ var Particle = require('particle-api-js');
 var particle = new Particle();
 var token = process.env.PARTICLE_TOKEN;
 
-function MongoDbUpdate(Data) {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db('HackVaccDb');
-        var myquery = { _id: 1 };
-        var newvalues = { $set: { _id: 1, PhotonData: Data } };
-        console.log(Data);
-        dbo.collection('appData').updateOne(myquery, newvalues, function (
-            err,
-            res
-        ) {
-            if (err) throw err;
-            console.log('1 document updated');
-            db.close();
-        });
-    });
-}
+particle
+    .login({
+        username: process.env.Iot_Email,
+        password: process.env.Iot_Password,
+    })
+    .then(
+        function (data) {},
+        function (err) {
+            console.log('Could not log in.', err);
+        }
+    );
 
-function MongoDbQuery() {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db('HackVaccDb');
-        var myquery = { _id: 1 };
-        dbo.collection('appData')
-            .find(myquery)
-            .toArray(function (err, result) {
+function MongoDbUpdate(Data, collection) {
+    MongoClient.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        function (err, db) {
+            if (err) throw err;
+            var dbo = db.db('HackVaccDb');
+            var myquery = { _id: 1 };
+            var newvalues = { $set: { _id: 1, PhotonData: Data } };
+            console.log(Data);
+            dbo.collection(collection).updateOne(myquery, newvalues, function (
+                err,
+                res
+            ) {
                 if (err) throw err;
-                console.log(result);
+                console.log('1 document updated');
                 db.close();
             });
-    });
+        }
+    );
+}
+
+function MongoDbQuery(collection) {
+    MongoClient.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        function (err, db) {
+            if (err) throw err;
+            var dbo = db.db('HackVaccDb');
+            var myquery = { _id: 1 };
+            dbo.collection(collection)
+                .find(myquery)
+                .toArray(function (err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    db.close();
+                });
+        }
+    );
 }
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.get('/get', function (req, res) {
-    MongoDbQuery();
-    res.send('Hello World!');
-});
+
 app.get('/photon', function (req, res) {
     particle
-        .login({ username: 'sushant.raj@uconn.edu', password: 'SushantPhoton' })
-        .then(
-            function (data) {},
-            function (err) {
-                console.log('Could not log in.', err);
-            }
-        );
-    particle
         .getVariable({
-            deviceId: '2c0034000f47363333343437',
+            deviceId: process.env.Device_Id,
             name: 'temp',
             auth: token,
         })
         .then(
             function (data) {
-                MongoDbUpdate(data);
-                res.send(data);
+                MongoDbUpdate(data, 'IotData');
                 console.log('Device variable retrieved successfully:', data);
             },
             function (err) {
                 console.log('An error occurred while getting attrs:', err);
             }
         );
+    MongoClient.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        function (err, db) {
+            if (err) throw err;
+            var dbo = db.db('HackVaccDb');
+            var myquery = { _id: 1 };
+            dbo.collection('IotData')
+                .find(myquery)
+                .toArray(function (err, result) {
+                    if (err) throw err;
+                    res.send(result);
+                    db.close();
+                });
+        }
+    );
 });
+
+app.get('/FeedbackUserReport', function (req, res) {});
+app.post('/FeedbackUserReport', function (req, res) {});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
